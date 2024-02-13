c134481747e4b47a1570985fcca89a03576e0aa1
Alex Trull <git-and-sig.alex@trull.org>
2024-02-13 17:12:16 +0100

    saner versions

--- versions.tf
+++ versions.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     hcp = {
       source  = "hashicorp/hcp"
-      version = "=>0.70.0"
+      version = "~>0.82.0"
     }
     aws = {
-      source  = "hashicorp/aws"
-      version = "=>5.0.0"
+      source = "hashicorp/aws"
     }
   }
 
