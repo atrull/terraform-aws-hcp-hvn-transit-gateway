f54d1eb9ebfb61a8e8f54af5fdd87f1d63f15fec
Alex Trull <git-and-sig.alex@trull.org>
2024-03-07 12:39:32 +0100

    attachment id

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "hcp_hvn_target_link" {
   description = "A unique URL identifying the HVN route."
   value       = join("", hcp_aws_transit_gateway_attachment.this.*.self_link)
 }
+
+output "hcp_hvn_target_link" {
+  description = "An attachment id for the transit gateway."
+  value       = join("", hcp_aws_transit_gateway_attachment.this.*.provider_transit_gateway_attachment_id)
+}
