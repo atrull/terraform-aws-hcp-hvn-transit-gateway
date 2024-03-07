3aa923eb75a18b689def35800d081aaaadc81c47
Alex Trull <git-and-sig.alex@trull.org>
2024-03-07 12:40:03 +0100

    correctly named output

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "hcp_hvn_target_link" {
   value       = join("", hcp_aws_transit_gateway_attachment.this.*.self_link)
 }
 
-output "hcp_hvn_target_link" {
+output "hcp_hvn_target_tgw_attachment_id" {
   description = "An attachment id for the transit gateway."
   value       = join("", hcp_aws_transit_gateway_attachment.this.*.provider_transit_gateway_attachment_id)
 }
